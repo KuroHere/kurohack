0ae9ff41d6e3ca68f871054c5327ecc7ad6aecde
KuroHere <83542909+KuroHere@users.noreply.github.com>
2021-05-05 14:07:56 +0700

    Delete bruh/kurohack/kurohack directory

--- bruh/kurohack/kurohack/api/util/HudUtil.java
+++
@@ -1,9 +0,0 @@
-package dev.kohimanayagato.serenity.api.util;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.client.Minecraft;
-
-public class HudUtil {
-
-}
