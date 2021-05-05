7e185a05dee02890180782cbc72297c3f38b111b
KuroHere <83542909+KuroHere@users.noreply.github.com>
2021-05-05 14:31:30 +0700

    Update Client.java

--- src/main/java/kuro/here/hack/Client.java
+++ src/main/java/kuro/here/hack/Client.java
@@ -1,12 +1,13 @@
-package dev.kohimanayagato.serenity;
+package kuro.here.hack;
 
-import dev.kohimanayagato.serenity.api.EventHandler;
-import dev.kohimanayagato.serenity.api.command.CommandManager;
-import dev.kohimanayagato.serenity.api.gui.clickgui.ClickGUI;
-import dev.kohimanayagato.serenity.api.manager.FriendManager;
-import dev.kohimanayagato.serenity.api.module.ModuleManager;
-import dev.kohimanayagato.serenity.api.setting.SettingManager;
-import dev.kohimanayagato.serenity.api.util.font.CustomFontRenderer;
+import kuro.here.hack.api.EventHandler;
+import kuro.here.hack.api.command.CommandManager;
+import kuro.here.hack.api.gui.clickgui.ClickGUI;
+import kuro.here.hack.api.manager.FriendManager;
+import kuro.here.hack.api.module.ModuleManager;
+import kuro.here.hack.api.setting.SettingManager;
+import kuro.here.hack.api.manager.PositionManager;
+import kuro.here.hack.api.util.font.CustomFontRenderer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -15,11 +16,12 @@ import dev.kohimanayagato.serenity.api.manager.ConfigManager;
 import java.awt.*;
 import java.io.IOException;
 
-@Mod(modid = "serenity", name = "Serenity", version = "0.4")
+@Mod(modid = "kurohack", name = "kurohack", version = "beta0.1")
 public class Client
 {
 	public static ModuleManager moduleManager;
 	public static SettingManager settingManager;
+        public static PositionManager positionManager;
 	public static CustomFontRenderer customFontRenderer;
 	public static ClickGUI clickGUI;
 	public static CommandManager commandManager;
@@ -29,6 +31,7 @@ public class Client
 	public void initialize(FMLInitializationEvent event) throws IOException {
 		commandManager = new CommandManager();
 		settingManager = new SettingManager();
+                positionManager = new PositionManager();
 		moduleManager = new ModuleManager();
 		friendManager = new FriendManager();
 		customFontRenderer = new CustomFontRenderer(new Font("Verdana", Font.PLAIN, 19), true, false);
